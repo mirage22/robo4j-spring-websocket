c9089cd17e1ba43650552e8b26a4a27a6c35862f
Miro Wengner <miro@robo4j.io>
2018-03-14 23:43:23 +0100

    cleanup

--- src/main/java/com/robo4j/example/traveler/spring/component/Robo4JServiceImpl.java
+++ src/main/java/com/robo4j/example/traveler/spring/component/Robo4JServiceImpl.java
@@ -4,13 +4,11 @@ import com.robo4j.RoboBuilder;
 import com.robo4j.RoboContext;
 import com.robo4j.RoboReference;
 import com.robo4j.example.traveler.model.GpsConfigMessage;
-import com.robo4j.example.traveler.spring.model.TravelerResponse;
 import com.robo4j.example.traveler.unit.GpsPointUnit;
 import com.robo4j.example.traveler.unit.TravelerUnit;
 import com.robo4j.spring.RoboSpringRegisterUnit;
 import com.robo4j.util.PropertyMapBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -24,13 +22,11 @@ public class Robo4JServiceImpl implements Robo4JService {
 
     private static final int DURATION_SECONDS = 1;
     private static final String ROBO4J_TRAVELER_XML = "robo4j_traveler.xml";
-    private final SimpMessagingTemplate template;
     private final TravelerService travelerService;
     private RoboContext roboSystem;
 
     @Autowired
-    public Robo4JServiceImpl(SimpMessagingTemplate template, TravelerService travelerService) {
-        this.template = template;
+    public Robo4JServiceImpl(TravelerService travelerService) {
         this.travelerService = travelerService;
     }
 
